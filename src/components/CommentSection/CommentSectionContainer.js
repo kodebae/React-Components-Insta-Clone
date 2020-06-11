ffa0c40ee299b013aba5b22b803be307220573ab
kodebae <karmenuopx@outlook.com>
2020-06-11 12:25:11 -0400

    finished likeSection

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -6,10 +6,15 @@ import "./Comment.css";
 
 const CommentSection = props => {
   // Add state for the comments
+const [comments] = useState(props.comments);
 
+console.log(props);
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
+      {comments.map((comment, index) => {
+        return <Comment comment={comment} key={index} />; 
+        })}
       <CommentInput />
     </div>
   );
